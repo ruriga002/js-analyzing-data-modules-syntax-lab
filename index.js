0596be417d892f2a98676201780963d51fe96173
ruriga002 <eruriga002@gmail.com>
2026-04-15 11:59:33 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+require(`datejs`);
 
+function combineUsers(...args) {
+    let combinedObject = {
+        users: []
+    };
+    for (let userArray of args) {
+        combinedObject.users.push(...userArray);
+    }
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+    return combinedObject;
 
-
+__};
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
